c5272dec51a2add367b208fc06103d7f0a02714b
si3nloong <sianloong90@gmail.com>
2021-06-14 00:13:48 +0800

    🙈 feat(base64): add `base64` util

---
+++ src/jvmMain/kotlin/util/Base64Factory.kt
@@ -0,0 +1,11 @@
+package org.rm.sdk.util
+
+import java.util.*
+
+actual object Base64Factory {
+    actual fun createEncoder(): Base64Encoder = JvmBase64Encoder
+}
+
+object JvmBase64Encoder : Base64Encoder {
+    override fun encode(src: ByteArray): ByteArray = Base64.getEncoder().encode(src)
+}
