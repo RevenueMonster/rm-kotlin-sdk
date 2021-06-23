29579972c0ca0e60dc8c32fff52d0a80b52dea62
si3nloong <sianloong90@gmail.com>
2021-06-23 17:03:33 +0800

    🐛 fix: signing issue (:signJvmPublication FAILED), GPG key format issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -186,9 +186,9 @@ publishing {
 }
 
 signing {
-//     useInMemoryPgpKeys(
-//         System.getenv("GPG_PRIVATE_KEY"),
-//         System.getenv("GPG_PRIVATE_PASSWORD")
-//     )
+    useInMemoryPgpKeys(
+        System.getenv("GPG_PRIVATE_KEY"),
+        System.getenv("GPG_PRIVATE_PASSWORD")
+    )
     sign(publishing.publications)
 }
