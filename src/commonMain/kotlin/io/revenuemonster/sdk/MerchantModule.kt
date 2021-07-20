cd99e3b223f6fb06e24f4318b83196fe233590f8
SnorSnor <snorsnor9998@gmail.com>
2021-07-20 11:10:48 +0800

    feat: get merchant profile

--- src/commonMain/kotlin/io/revenuemonster/sdk/MerchantModule.kt
+++ src/commonMain/kotlin/io/revenuemonster/sdk/MerchantModule.kt
@@ -3,19 +3,25 @@ package io.revenuemonster.sdk
 import io.revenuemonster.sdk.model.Item
 import io.revenuemonster.sdk.model.Items
 import io.revenuemonster.sdk.model.common.Store
+import io.revenuemonster.sdk.model.response.MerchantProfile
 import io.revenuemonster.sdk.model.response.MerchantSubscriptions
 
 class MerchantModule(private val sdk: RevenueMonsterSDK) {
 
     private val version = "v3"
 
-    suspend fun getSubscriptions(): Items<MerchantSubscriptions> {
-        return sdk.call<Any,Items<MerchantSubscriptions>>(
+    //having problem -> item : List<T>
+    suspend fun getSubscriptions(): Item<MerchantSubscriptions> {
+        return sdk.call<Any,Item<MerchantSubscriptions>>(
             url = "/$version/merchant/subscriptions"
         )
     }
 
-//    suspend fun getStores(): Items<Store> {
-//        return sdk.call<Any, Items<Store>>("/v3/stores")
-//    }
+    suspend fun getProfile() : Item<MerchantProfile>{
+        return sdk.call<Any,Item<MerchantProfile>>(
+            url = "/$version/merchant"
+        )
+    }
+
+
 }
