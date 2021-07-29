d46a4c8fb04f1444a71695b1be6cb65e1e5531c8
SnorSnor <snorsnor9998@gmail.com>
2021-07-29 09:33:03 +0800

    fix: daily settlement report

--- src/commonMain/kotlin/io/revenuemonster/sdk/model/request/DailySettlementReportRequest.kt
+++ src/commonMain/kotlin/io/revenuemonster/sdk/model/request/DailySettlementReportRequest.kt
@@ -4,10 +4,10 @@ import io.revenuemonster.sdk.model.enum.TransactionType
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class DailySettlementReportRequest (
-    val transactionType : String,
-    val date : String,
-    val method : String,
-    val region : String,
-    val cursor : String
-    )
+data class DailySettlementReportRequest(
+    val transactionType: String,
+    val date: String,
+    val method: List<String>,
+    val region: List<String>,
+    val cursor: String
+)
