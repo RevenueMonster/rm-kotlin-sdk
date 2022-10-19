0c3baed2fbc8d6fcd1beec850c135983c1605fba
SnorSnor <snorsnor9998@gmail.com>
2022-10-19 15:56:34 +0800

    chore: remove unused class

--- src/commonMain/kotlin/io/revenuemonster/sdk/model/common/PhoneNumber.kt
+++
@@ -1,9 +0,0 @@
-package io.revenuemonster.sdk.model.common
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class PhoneNumber(
-    val countryCode: String,
-    val phoneNumber: String,
-)
